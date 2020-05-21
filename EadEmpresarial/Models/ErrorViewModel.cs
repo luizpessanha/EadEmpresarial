c0ef047e58510f534e6ae9bf1128def42ded2d6a
Luiz Pessanha <luizcarp@gmail.com>
2020-05-21 10:42:09 -0300

    Adicionar arquivos de projeto.

---
+++ EadEmpresarial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EadEmpresarial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
